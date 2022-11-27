489c32a9421fee6a340d6d5a5aa897edfc08b327
Larry66666 <81087922+Larry66666@users.noreply.github.com>
2022-11-28 01:20:18 +0800

    Add files via upload

---
+++ src/views/card/Promotion.vue
@@ -0,0 +1,30 @@
+<template>
+    <el-card class = "box-card" shadow = "never">
+         <div slot  = "header">
+             <span>🍻推广</span>
+         </div>
+         <div>
+            body
+         </div>
+     </el-card> 
+ </template>
+ 
+ 
+ <script>
+
+ export default {
+   name: 'Promorion',
+   data() {
+     return {
+       
+     }
+   },
+   created() {
+      
+   },
+   methods : {
+     
+   }
+   
+ }
+ </script>
