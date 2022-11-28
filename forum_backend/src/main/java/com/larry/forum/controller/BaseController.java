5bdc5fdead960f17d1a59dbd61eccb564d8d4276
Larry66666 <81087922+Larry66666@users.noreply.github.com>
2022-11-28 12:58:55 +0800

    Delete forum_backend directory

--- forum_backend/src/main/java/com/larry/forum/controller/BaseController.java
+++
@@ -1,4 +0,0 @@
-package com.larry.forum.controller;
-
-public class BaseController {
-}
--- forum_backend/src/main/java/com/larry/forum/mapper/BmsBillboardMapper.java
+++
@@ -1,9 +0,0 @@
-package com.larry.forum.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.larry.forum.model.entity.BmsBillboard;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BmsBillboardMapper extends BaseMapper<BmsBillboard> {
-}
