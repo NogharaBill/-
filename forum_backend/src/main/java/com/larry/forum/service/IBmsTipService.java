fd461bdb78478b864ae343b3e97c3c534dfdfede
Larry66666 <81087922+Larry66666@users.noreply.github.com>
2022-11-30 21:37:18 +0800

    Delete forum_backend directory

--- forum_backend/src/main/java/com/larry/forum/service/IBmsTipService.java
+++
@@ -1,8 +0,0 @@
-package com.larry.forum.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.larry.forum.model.entity.BmsTip;
-
-public interface IBmsTipService extends IService<BmsTip> {
-    BmsTip getRandomTip();
-}
