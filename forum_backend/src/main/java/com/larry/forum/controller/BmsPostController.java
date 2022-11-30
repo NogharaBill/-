879169c40ec97e2c9498bdb48e572790f9545209
NogharaBill <1207163867@qq.com>
2022-11-30 14:36:02 +0800

    帖子列表，发布帖子和浏览帖子的界面

--- forum_backend/src/main/java/com/larry/forum/controller/BmsPostController.java
+++ forum_backend/src/main/java/com/larry/forum/controller/BmsPostController.java
@@ -8,19 +8,12 @@ import com.larry.forum.model.entity.UmsUser;
 import com.larry.forum.model.vo.PostVO;
 import com.larry.forum.service.IBmsPostService;
 import com.larry.forum.service.IUmsUserService;
-import com.vdurmont.emoji.EmojiParser;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
-import javax.validation.Valid;
 
 import java.util.Map;
-import java.util.Date;
 
 import static com.larry.forum.jwt.JwtUtil.USER_NAME;
-
-
-
 
 @RestController
 @RequestMapping("/post")
@@ -54,27 +47,5 @@ public class BmsPostController extends BaseController {
         System.out.println("这是一个map");
         System.out.println(map);
         return ApiResult.success(map);
-    }
-
-    /*帖子更新、修改*/
-    @PostMapping("/update")
-    public ApiResult<BmsPost> update(@RequestHeader(value = USER_NAME) String userName, @Valid @RequestBody BmsPost post) {
-        UmsUser umsUser = umsUserService.getUserByUsername(userName);
-        Assert.isTrue(umsUser.getId().equals(post.getUserId()), "非本人无权修改");
-        post.setModifyTime(new Date());
-        post.setContent(EmojiParser.parseToAliases(post.getContent()));
-        iBmsPostService.updateById(post);
-        return ApiResult.success(post);
-    }
-
-    /*帖子删除*/
-    @DeleteMapping("/delete/{id}")
-    public ApiResult<String> delete(@RequestHeader(value = USER_NAME) String userName, @PathVariable("id") String id) {
-        UmsUser umsUser = umsUserService.getUserByUsername(userName);
-        BmsPost byId = iBmsPostService.getById(id);
-        Assert.notNull(byId, "来晚一步，话题已不存在");
-        Assert.isTrue(byId.getUserId().equals(umsUser.getId()), "你为什么可以删除别人的话题？？？");
-        iBmsPostService.removeById(id);
-        return ApiResult.success(null,"删除成功");
     }
 }
